17b6030194a350a3a06817a2294b4fe69be88b2f
ijohnnyh <ijohnnyh@live.unc.edu>
2017-05-19 10:14:29 -0400

    Added POST method for company name

--- init.py
+++ init.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import scraping/master_parse
+
 app = Flask(__name__)
 
 
@@ -14,3 +16,9 @@ def date(date):
 
 if __name__ == "__main__":
     app.run()
+
+@app.route('/newCompany', methods = ['POST'])
+def newCompany():
+    company = request.form['companyName']
+    print(company)
+    return render_template('docs/indexWithGraph.html')
