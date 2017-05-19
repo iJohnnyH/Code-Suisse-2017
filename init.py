2c7b35687f562fc45bbde5c44405752c9a1b0b9b
Dennis <dsun96@gmail.com>
2017-05-19 01:49:21 -0500

    calendar update, doesnt work currently

--- init.py
+++ init.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
+
+
 @app.route("/")
 def index():
-    return render_template("docs/index.html")
-    
+    return render_template("docs/indexNoGraph.html")
+
+
+@app.route("/<date>")
+def date(date):
+    print(date)
+    return render_template("docs/indexWithGraph.html", date=date)
+
 if __name__ == "__main__":
     app.run()
