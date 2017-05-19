c3431e9925bb4d1c6037a4f2abb8f3bfe9882f1a
ijohnnyh <ijohnnyh@live.unc.edu>
2017-05-19 02:46:37 -0400

    modified readme and updated parser

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#Code Suisse 2017
+# Code Suisse 2017
 
 Project created by Johnny Huang, Dennis Sun, and Justin Ledinh on behalf of Credit Suisse's Code Suisse 2017 Hackathon project #7, stock price prediction.
--- scraping/eventParser.py
+++ scraping/eventParser.py
@@ -10,16 +10,12 @@ parser = soup.findAll("div", {"class" : "bld date"})
 eventDates = []
 for x in range (0 , len(parser)):
 	eventDates.append(parser[x].text)
-print (eventDates)
-
-print("\n")
 
 parser = soup.findAll("div", {"class": "event"})
 subParser = []
 eventTitles = []
 for x in range (0 , len(parser)):
 	subParser.append(parser[x].findAll("div" , {"class" : None}))
-
 string = ""
 for x in range(0 , len(subParser)):
 	eventTitles.append(subParser[x][0].text.rstrip())
