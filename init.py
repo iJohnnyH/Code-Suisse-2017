2a9477405138d19e7fd6cd8a54582b1f936f5fd8
dennissun1 <dsun96@gmail.com>
2017-05-19 10:06:41 -0500

    Update init.py

--- init.py
+++ init.py
@@ -1,15 +1,61 @@
 from flask import Flask, render_template, request
-import master_parse
+import master_parse, html
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("docs/indexNoGraph.html")
+    company = "AAPL"
+    file = urlopen("http://www.google.com/finance?q=" + company)
+    text = file.read()
+    soup = BeautifulSoup(text, "lxml")
+    parser = soup.findAll("div", {"class" : "bld date"})
+    eventDates = []
+
+    for x in range (0 , len(parser)):
+        eventDates.append(parser[x].text)
+
+    parser = soup.findAll("div", {"class": "event"})
+    subParser = []
+    eventTitles = []
+    masterString= ""
+    for x in range (0 , len(parser)):
+        subParser.append(parser[x].findAll("div" , {"class" : None}))
+    string = ""
+    for x in range(0 , len(subParser)):
+        eventTitles.append(subParser[x][0].text.rstrip())
+    for x in range(0, len(parser)):
+        eventTitles[x] += eventTitles[x]+": "+eventDates[x]+"---------" 
+    return render_template("docs/indexNoGraph.html", eventTitles=eventTitles)
 
 
 @app.route("/<date>")
 def date(date):
+    
+    company = "AAPL"
+    file = urlopen("http://www.google.com/finance?q=" + company)
+    text = file.read()
+    soup = BeautifulSoup(text, "lxml")
+    parser = soup.findAll("div", {"class" : "bld date"})
+    eventDates = []
+
+    for x in range (0 , len(parser)):
+        eventDates.append(parser[x].text)
+
+    parser = soup.findAll("div", {"class": "event"})
+    subParser = []
+    eventTitles = []
+    masterString= ""
+    for x in range (0 , len(parser)):
+        subParser.append(parser[x].findAll("div" , {"class" : None}))
+    string = ""
+    for x in range(0 , len(subParser)):
+        eventTitles.append(subParser[x][0].text.rstrip())
+    for x in range(0, len(parser)):
+        eventTitles[x] += eventTitles[x]+": "+eventDates[x]+"---------" 
+    
     datef = date.split(" ")
     year = datef[3]
     day = datef[2]
@@ -21,7 +67,7 @@ def date(date):
     else:
     	month = str(month)
     master_parse.main(year+"-"+month+"-"+day,"NASDAQ","AAPL")
-    return render_template("docs/indexWithGraph.html", date=date)
+    return render_template("docs/indexWithGraph.html", date=date, eventTitles=eventTitles)
 
 if __name__ == "__main__":
     app.run()
